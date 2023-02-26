5dee9b2c1b5aac25e7a501600c269d6643dc0793
albertomoiseisdev <contatoalbertomoiseis@gmail.com>
2023-02-26 01:28:20 -0300

    Primeiro Commit

---
+++ src/com/br/fundamentos/PrimeiroProjeto.java
@@ -0,0 +1,10 @@
+package com.br.fundamentos;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Moiseis
+ *
+ */
+module Java {
+}
